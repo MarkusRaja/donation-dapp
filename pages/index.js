67d0287746f54cf546af5dd7356bdc41db721b3d
Mark Sinabutar <markus.sinabutar@student.president.ac.id>
2023-04-27 22:17:55 +0700

    minor fix 1

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Dropdown from "react-bootstrap/Dropdown";
-import { getDefaultProvider, ethers } from "ethers";
 import Web3 from 'web3';
-import {connectD, fetchD, detectNetwork1} from "../services/aio";
-import { Fragment, useState, useEffect } from 'react';
-import { Grid, GridItem, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, Button, ChakraProvider, Image, Select  } from '@chakra-ui/react'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
-
-const projectId = '2J5pUVzx4zKsQTgwnAYsE6MVObL'
-const projectSecret = 'f41fe913f0ae1ea7c45c0d36caace45a'
-const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
-const client = ipfsHttpClient({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https',
-  apiPath: '/api/v0',
-  headers: {
-    authorization: auth
-  }
-})
+import { useState, useEffect } from 'react';
+import { ChakraProvider, Select } from '@chakra-ui/react'
 export default function Home({connect, address, syncW, currencyD}) {
   const [filterC, setFilterC] = useState('option1');
   const [allPosts, setPosts] = useState([]);
